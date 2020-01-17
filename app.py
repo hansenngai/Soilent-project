db7a808858b0cef2fb0de018e3ad3512fbb7c4a8
Hansen Ngai <ngaih1@mcmaster.ca>
2020-01-17 01:13:23 -0500

    project
    project

---
+++ app.py
@@ -0,0 +1,108 @@
+from flask import Flask, jsonify, make_response
+import requests
+from flask_cors import CORS
+from datetime import datetime
+import json, pandas
+from predict import get_prediction
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def home():
+    return "<h1>Hullo</h1>"
+
+
+@app.route('/predict/<uid>')
+def predict(uid):
+    return jsonify({'prediction': get_prediction(datetime.now())})
+
+def squish_health(health):
+    minimum, maximum = 0, 100
+    health = ((maximum - minimum) * (health - minimum)) // (1000)
+    return health
+
+@app.route('/update-realtime/<uid>/<health>')
+def update_realtime(uid, health):
+    health = squish_health(int(health))
+    with open("store.json") as file:
+        store = json.load(file)
+    if not store:
+        store = {}
+    store['users'][str(uid)] = health
+    with open("store.json", "w") as file:
+        json.dump(store, file)
+    return make_response(jsonify({}))
+
+
+@app.route('/update/<uid>/<health>')
+def update(uid, health):
+    health = squish_health(int(health))
+    df = pandas.read_json("data.json")
+    temptime = datetime.now()
+    time = temptime.isoformat().replace('T', ' ')
+    time = time[:time.index('.')]
+    df2 = pandas.DataFrame([[time, health]],  columns=['ds', 'y'])
+    df = df.append(df2, ignore_index = True)
+    df.to_json("data.json")
+    ################################################################################
+    # FIREBASE
+    with open("data.json") as f:
+        content = json.load(f)
+
+    url = "https://mhacks12-c37e8.firebaseio.com/users/.json"
+    payload = content
+    headers = {
+        'Content-Type': "application/json",
+        'User-Agent': "PostmanRuntime/7.17.1",
+        'Accept': "*/*",
+        'Cache-Control': "no-cache",
+        'Postman-Token': "e7e1ce3d-c916-44fa-b94b-1b61bd6cb630,baf09016-fb5f-4fe6-899a-d1c0615ba294",
+        'Host': "mhacks12-c37e8.firebaseio.com",
+        'Accept-Encoding': "gzip, deflate",
+        'Content-Length': "36",
+        'Connection': "keep-alive",
+        'cache-control': "no-cache"
+    }
+
+    # response = requests.request("POST", url, data=payload, headers=headers)
+    response = requests.put(url, json=payload, headers=headers)
+    return make_response(jsonify({}))
+
+
+@app.route('/status/<uid>')
+def status(uid):
+    with open("store.json") as file:
+        store = json.load(file)
+    health = store['users'][str(uid)]
+    import random
+    health_matrix = [
+        [health]*5,
+        [health-(random.randint(5, 15))]*5,
+        [health-(random.randint(5, 15))]*5,
+        [health-(random.randint(5, 15))]*5,
+        [health-(random.randint(5, 15))]*5
+    ]
+    # 5x5
+    # [1,1,1,1,1]
+    # [1,1,1,1,1]
+    # [1,1,1,0,1]
+    # [1,1,1,1,1]
+    # [1,1,1,1,1]
+    sensor_x = 3
+    sensor_y = 2
+    for row, row_vals in enumerate(health_matrix):
+        for col, location in enumerate(row_vals):
+            if row == sensor_y and col == sensor_x:
+                health_matrix[row][col] = health
+            else:
+                euclidean_distance = (row - sensor_y)**2 + (col - sensor_x)**2
+                health_matrix[row][col] = health - 3*euclidean_distance
+
+    response = make_response(jsonify({'health': health_matrix}))
+    return response
+
+
+if __name__ == '__main__':
+    app.run(port = 5006)
---
+++ predict.py
@@ -0,0 +1,56 @@
+import fbprophet
+import pandas
+import requests
+import matplotlib.pyplot as plt
+import csv
+from datetime import datetime, timedelta
+
+THRESHOLD = 120
+per = 120
+
+def get_prediction(timestamp, predict = True):
+    forecast = pandas.read_json('forecast.json')
+    if predict:
+        data = requests.get("https://mhacks12-c37e8.firebaseio.com/users/.json")
+        df = pandas.read_json(data.content)
+        m = fbprophet.Prophet()
+        m.fit(df)
+        future = m.make_future_dataframe(periods=per, freq="H")
+        forecast = m.predict(future)
+        forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+        m.plot(forecast)
+        #plt.show()
+        forecast.to_json("forecast.json")
+    prediction, hour = None, None
+    distance = float("inf")
+    for time in forecast.ds[per:]:
+        ts_hour = timestamp.hour
+        t_hour = time.hour
+        diff = ts_hour - t_hour if ts_hour > t_hour else t_hour - ts_hour
+        if diff < distance:
+            distance = diff
+            hour = time.hour
+    for hour, health in enumerate(forecast.yhat if predict else forecast.y):
+        if health < THRESHOLD:
+            return hour
+
+if __name__ == '__main__':
+    get_prediction(datetime.now())
+    pass
+
+import random
+data = [["ds", "y"]]
+for date in ("07", "08", "09", "10", "11"):
+    health = 200
+    for hour in ("00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"):
+        data.append(["2019-10-" + date + " " + hour + ":00:00", health])
+        health -= random.randrange(3, 7)
+with open("data.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerows(data)
+
+
+
+
+
+
